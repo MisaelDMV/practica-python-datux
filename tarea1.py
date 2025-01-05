91c502243b5ce0ffadd808d6f2ddd786019a9350
Misael Dayron <nitrox748@gmail.com>
2025-01-05 15:36:56 -0500

    Add files via upload

---
+++ tarea1.py
@@ -0,0 +1,11 @@
+print("hola mundo")
+
+edadBase=18
+
+edadDeusuario=int(input("¿Cuantos años tienes?: "))
+
+if edadDeusuario>=edadBase:
+   print("Simon a la cacarisa")
+
+else:
+   print("usted tiene la edad de un fan de plin plin")
